636dace6d9e02b93e07dbbad209cbb27b717991b
Ricardo Batista <ricardo.batista@olx.com>
2018-01-31 11:02:10 +0100

    New Event store

---
+++ src/store/EventStore.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ShoppingKart\Store;
+
+class EventStore
+{
+    private $metaevents = [];
+
+    public function __construct($metaevents)
+    {
+        $this->metaevents = $metaevents;
+    }
+
+    public function getEvents()
+    {
+        return $this->metaevents;
+    }
+
+    public function addEvent($event)
+    {
+        $this->metaevents[] = $event;
+    }
+
+    public static function fromFile()
+    {
+        $filename = dirname(__dir__) . '/resources/events.json';
+        $handle = fopen($filename, 'r');
+        $contents = fread($handle, filesize($filename));
+        fclose($handle);
+        return new EventStore(json_decode($contents, true));
+    }
+
+    public function flush()
+    {
+        $filename = dirname(__dir__) . '/resources/events.json';
+        $handle = fopen($filename, 'w+');
+        fwrite($handle, json_encode($this->metaevents));
+        fclose($handle);
+    }
+}
